2b5e5325213c19288fb0f5e0ff69aee03a6cc7ae
Earvin Li <earvin.tli@gmail.com>
2017-11-25 10:48:32 -0800

    one secret of generating the table

--- js/app.js
+++ js/app.js
@@ -12,6 +12,7 @@ function makeGrid() {
   for (i = 0; i < $('#input_height').val(); i++) {
       $('#pixel_canvas').append('<tr></tr>');
       for (j = 0; j < $('#input_width').val(); j++) {
+          // the use of ':last' is the key of generating the table
           $('tr:last').append('<td class="white"></td>');
       }
   }
